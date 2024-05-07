5f7aa67ce160656b27690a5937740b892310df69
vishnuprasad kannoth <vishnuvpk20@gmail.com>
2024-05-07 10:23:52 +0100

    fixing

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('/landing_page', views.landing_page, name='landing_page'),
+    path('/landing_page', views.landing_page, name='blog-home'),
     path('data_table/', views.data_table, name='data_table'),
 ]
