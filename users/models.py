b04f520cf52353f0c4f4363126833a6824c090a6
vishnuprasad kannoth <vishnuvpk20@gmail.com>
2024-05-20 10:28:38 +0100

    all final changes

--- users/models.py
+++ users/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):
+    def _str_(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        super().save(force_insert=force_insert, force_update=force_update, *args, **kwargs)
 
         img = Image.open(self.image.path)
 
--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -15,6 +14,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
 
 
 @login_required
